895146605c26de2a3916567cdb022f3d66bd8c64
Subash Bhujel <subasb@microsoft.com>
2016-07-18 13:12:34 -0700

    adding gulp-open and bootstrap packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,17 @@
 var gulp = require('gulp'),
-    nodemon = require('gulp-nodemon');
+    nodemon = require('gulp-nodemon'),
+    open = require('gulp-open'); // open a file in web brower
 
-gulp.task('default', function() {
+// Opens the browser
+gulp.task('open', function() {
+    gulp.src('public/index.html')
+        .pipe(open({
+            uri: 'http://localhost:4001/'
+        }));
+});
+
+// Default gulp task
+gulp.task('build', function() {
     nodemon({
         script: 'app.js',
         ext: 'js html css',
@@ -10,3 +20,6 @@ gulp.task('default', function() {
         console.log('Changes detected. Restarting the server.')
     });
 });
+
+// Default task that will run when 'gulp' is run from git bash
+gulp.task('default', ['build', 'open']);
